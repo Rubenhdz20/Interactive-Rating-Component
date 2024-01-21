848b03a4b15e09498ea8a50a3510b274fe081712
Rubenhdz20 <rubhernandez42@gmail.com>
2024-01-20 18:45:42 -0600

    Improve js and css

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-
 function getRating() {
-    var rateButton = document.getElementsByClassName("rate-number");
-    var rate = "";
-    for (var i = 0; i < rateButton.length; i++) {
-      var button = rateButton[i];
-      button.addEventListener("click", function onClick(event) {
-        rate = event.target.value;
-        console.log(rate);
-        submitRating(rate);
-      });
-    }
+    let rateButtons = document.querySelectorAll(".rate-number");
+
+    rateButtons.forEach(function(button) {
+        button.addEventListener("click", function onClick(event) {
+            var rate = event.target.value;
+            console.log(rate);
+            submitRating(rate);
+        });
+    });
 }
 
 function submitRating(rate) {
     let submitButton = document.querySelector("#rating-submit");
-    submitButton.addEventListener('click', function(event) {
-        let originalHtmlContainer = document.querySelector("#rating-state");
-        let text = `You selected ${rate} out of 5`;
-        let thankYouHtmlContainer =
-        `<div id="thanks-state" class="thanks-container">
-            <picture>
-                <img class="thanks-img" src="./interactive-rating-component-main/images/illustration-thank-you.svg" alt="Thank you!">
-            </picture>
-            <p id="submit" class="btn-submit-thanks">${text}</p>
-            <h1>Thank you!</h1>
-            <p class="thanks-text">
-                We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
-            </p>
-            <button id="reload" type="button" class="appear" onClick="returnPage()">Reset</button>
-        </div>`;
-        originalHtmlContainer.innerHTML = thankYouHtmlContainer;
-    });  
+    submitButton.addEventListener('click', function() {
+        var originalHtmlContainer = document.querySelector("#rating-state");
+        var text = `You selected ${rate} out of 5`;
+        var thankYouHtmlContainer = `
+            <div id="thanks-state" class="thanks-container">
+                <picture>
+                    <img class="thanks-img" src="./interactive-rating-component-main/images/illustration-thank-you.svg" alt="Thank you!">
+                </picture>
+                <p id="submit" class="btn-submit-thanks">${text}</p>
+                <h1>Thank you!</h1>
+                <p class="thanks-text">
+                    We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
+                </p>
+                <button id="reload" type="button" class="appear" onClick="returnPage()">Reset</button>
+            </div>`;
 
+        originalHtmlContainer.innerHTML = thankYouHtmlContainer;
+    });
 }
 
 function returnPage() {
@@ -41,8 +39,4 @@ function returnPage() {
     });
 }
 
-console.log(getRating());
-
-console.log(submitRating());
-
-console.log(returnPage());
+getRating();
